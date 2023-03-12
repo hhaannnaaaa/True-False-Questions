f7eb972a226c70d2a3f7093f102b32dd1cc8dae5
hhaannnaaaa <hannalovisaberg@gmail.com>
2023-03-12 20:07:02 +0000

    Add array with questioins and answers in js file

---
+++ assets/css/script.js
@@ -0,0 +1,47 @@
+// Array with questions and answers and current question variable to write out the question
+
+var currentQuestion = 0;
+var questions = [
+    {
+        "question": "You can sneeze during your sleep.",
+        "answer": False
+    },
+    {
+        "question": "Victoria Beckham had a cameo appearance in the original Sex and the City series.",
+        "response": "It was actually Geri Halliwell who appeared as Samantha's friend Phoebe.",
+        "answer": False
+    },
+    {
+        "question": "Before becoming queen, Queen Elizabeth II trained as a mechanic.",
+        "answer": True
+    },
+    {
+        "question": "The first music video to be played on MTV was Video Killed The Radio Star by The Buggles in 1981.",
+        "answer": True
+    },
+    {
+        "question": "Rihanna was born in the Bahamas.",
+        "response": "She is actually from Barbados.",
+        "answer": False
+    },
+    {
+        "question": "The first animal sent into space was a monkey.",
+        "response": "Fruit flies were sent into space on a V-2 rocket in 1947.",
+        "answer": False
+    },
+    {
+        "question": "Singer Billie Eilish’s full name is Billie Eilish Pirate Baird O’Connell.",
+        "answer": True
+    },
+    {
+        "question": "Emma Roberts is the daughter of Julia Roberts.",
+        "response": "Julia Roberts is her aunt: her father is fellow actor Eric Roberts.",
+        "answer": True
+    },
+    {
+        "question": "Humans lose an average of 75 hairs from their head each month.",
+        "response": "We can actually lose up to 75 strands of hair each DAY!",
+        "answer": False
+    },
+
+]
