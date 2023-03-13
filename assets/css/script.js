abb0654f386782cb2c1cba2b63e51b60ec1b3ed3
hhaannnaaaa <hannalovisaberg@gmail.com>
2023-03-13 19:27:27 +0000

    Add DOM to js file

--- assets/css/script.js
+++ assets/css/script.js
@@ -4,20 +4,20 @@ var currentQuestion = 0;
 var questions = [
     {
         "question": "You can sneeze during your sleep.",
-        "answer": False
+        "answer": false
     },
     {
         "question": "Victoria Beckham had a cameo appearance in the original Sex and the City series.",
         "response": "It was actually Geri Halliwell who appeared as Samantha's friend Phoebe.",
-        "answer": False
+        "answer": false
     },
     {
         "question": "Before becoming queen, Queen Elizabeth II trained as a mechanic.",
-        "answer": True
+        "answer": true
     },
     {
         "question": "The first music video to be played on MTV was Video Killed The Radio Star by The Buggles in 1981.",
-        "answer": True
+        "answer": true
     },
     {
         "question": "Rihanna was born in the Bahamas.",
@@ -27,21 +27,46 @@ var questions = [
     {
         "question": "The first animal sent into space was a monkey.",
         "response": "Fruit flies were sent into space on a V-2 rocket in 1947.",
-        "answer": False
+        "answer": false
     },
     {
         "question": "Singer Billie Eilish’s full name is Billie Eilish Pirate Baird O’Connell.",
-        "answer": True
+        "answer": true
     },
     {
         "question": "Emma Roberts is the daughter of Julia Roberts.",
         "response": "Julia Roberts is her aunt: her father is fellow actor Eric Roberts.",
-        "answer": True
+        "answer": true
     },
     {
         "question": "Humans lose an average of 75 hairs from their head each month.",
         "response": "We can actually lose up to 75 strands of hair each DAY!",
-        "answer": False
+        "answer": false
     },
 
 ]
+
+(function() {
+    functionReset();
+
+document.getElementById('next-question').addEventLinstener('click', function(event) {
+    fnCheck();
+});
+
+document.getElementById('checktrue').addEventLinstener('click', function(event) {
+    fnCheck(true);
+});
+
+document.getElementById('checkfalse').addEventLinstener('click', function(event) {
+    fnCheck(false);
+});
+
+document.getElementById('next-question').addEventLinstener('click', function(event) {
+    fnNext();
+});
+
+document.getElementById('clear').addEventLinstener('click', function(event) {
+    fnReset();
+});
+
+})();
